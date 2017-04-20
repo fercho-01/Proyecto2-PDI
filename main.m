8078d4b77ffaee8db39a8b673e314f8489695034
fercho-01 <luisfernandorzcrzc@gmail.com>
2017-04-20 12:10:54 -0500

    first commit

---
+++ main.m
@@ -0,0 +1,34 @@
+clear all;close all;clc;
+%Cargar imagen original
+a = imread('DSC_2663.jpg');
+%cargar mascara
+b = imread('mascara2.jpg');
+
+%usar la mascara para seleccionar la región de interes en la imagen
+%original
+b(b>0)=255;
+a(b==255)=0;
+
+%recortar la imagen original
+[fil,col] = find(rgb2gray(a)>0);
+fmin = min(fil(:));
+fmax = max(fil(:));
+cmin = min(col(:));
+cmax = max(col(:));
+c = a(fmin:fmax,cmin:cmax,:);
+figure(1);
+imshow(c);impixelinfo();
+
+%extraer los canales r,g,b de la imagen
+r = c(:, :, 1);
+g = c(:, :, 2);
+b = c(:, :, 3);
+
+%aplicar median filter a cada canal de la imagen
+redMF = medfilt2(r, [40 40]);
+greenMF = medfilt2(g, [40 40]);
+blueMF = medfilt2(b, [40 40]);
+
+%reconstrucción de la imagen
+rgbFixed = cat(3, redMF, greenMF, blueMF);
+figure(2);imshow(rgbFixed);
