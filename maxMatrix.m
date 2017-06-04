2594c04e0500a48e26bc90d2ae9ede6d974c5bfa
fercho-01 <luisfernandorzcrzc@gmail.com>
2017-06-04 17:25:22 -0500

    comentar maxMatrix, minMatrix

--- maxMatrix.m
+++ maxMatrix.m
@@ -1,16 +1,34 @@
+%----------------------------------
+%   Tarea 3, maxMatrix
+%       - Luis Fernando Orozco (lfernando.orozco@udea.edu.co)
+%         Estudiante de Ingeniería de Sistemas, Udea
+%         CC 1216716983
+%       - Santiago Sanmartin (santiago.sanmartin@udea.edu.co)
+%         Estudiante de Ingenieria de Sistemas, Udea
+%         CC 1017209945
+%       V1 abril 2017
+
+%Esta función halla el valor maximo de una matriz
 function [NM,F,C]=maxMatrix(M)
-[tf,tc]=size(M);
-NM=M(1,1);
-F=1;
-C=1;
-for i=1:tf
-for j=1:tc
-if M(i,j)>NM
-NM=M(i,j);
-F=i;
-C=j;
-end
-end
+    
+    %Se obtienen las dimensiones de la matriz
+    [tf,tc]=size(M);
+    NM=M(1,1); %Primer elemento de la matriz
+    F=1;
+    C=1;
+    
+    %Se recorre la matriz
+    for i=1:tf
+        for j=1:tc
+        %Se compara el valor maximo hasta ahora con el actual
+        if M(i,j)>NM
+            %Si el actual es mayor que el que se tenia, se actualiza el
+            %valor maximo
+            NM=M(i,j);
+            F=i;
+            C=j;
+        end
+    end
 end
 
 
